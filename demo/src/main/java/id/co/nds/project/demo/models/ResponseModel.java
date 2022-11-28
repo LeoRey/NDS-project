065a1b32b408e04a0af36eeb0823013173888252
KC489 <kchrist773939@gmail.com>
2022-11-28 14:57:55 +0700

    responseModel init

---
+++ demo/src/main/java/id/co/nds/project/demo/models/ResponseModel.java
@@ -0,0 +1,40 @@
+package id.co.nds.project.demo.models;
+
+public class ResponseModel {
+  private String responseCode;
+  private String responseMessage;
+  private String responseDescription;
+  private String responseTime;
+
+  public String getResponseCode() {
+    return responseCode;
+  }
+
+  public void setResponseCode(String responseCode) {
+    this.responseCode = responseCode;
+  }
+
+  public String getResponseMessage() {
+    return responseMessage;
+  }
+
+  public void setResponseMessage(String responseMessage) {
+    this.responseMessage = responseMessage;
+  }
+
+  public String getResponseDescription() {
+    return responseDescription;
+  }
+
+  public void setResponseDescription(String responseDescription) {
+    this.responseDescription = responseDescription;
+  }
+
+  public String getResponseTime() {
+    return responseTime;
+  }
+
+  public void setResponseTime(String responseTime) {
+    this.responseTime = responseTime;
+  }
+}
